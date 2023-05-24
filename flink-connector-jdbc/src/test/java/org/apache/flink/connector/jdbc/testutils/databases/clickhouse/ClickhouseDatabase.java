036a67fafa01f4d22a8f239b8a3252bdb38ce698
wending <1062698930@qq.com>
2023-05-25 01:12:10 +0800

    [FLINK-32068]connector jdbc support clickhouse

---
+++ flink-connector-jdbc/src/test/java/org/apache/flink/connector/jdbc/testutils/databases/clickhouse/ClickhouseDatabase.java
@@ -0,0 +1,55 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.connector.jdbc.testutils.databases.clickhouse;
+
+import org.apache.flink.connector.jdbc.testutils.DatabaseExtension;
+import org.apache.flink.connector.jdbc.testutils.DatabaseMetadata;
+import org.apache.flink.util.FlinkRuntimeException;
+
+import org.testcontainers.containers.ClickHouseContainer;
+
+/** A Clickhouse database for testing. */
+public class ClickhouseDatabase extends DatabaseExtension implements ClickHouseImages {
+
+    private static final ClickHouseContainer CONTAINER =
+            new ClickHouseContainer(CLICKHOUSE_IMAGE_23);
+
+    private static ClickHouseMetadata metadata;
+
+    public static ClickHouseMetadata getMetadata() {
+        if (!CONTAINER.isRunning()) {
+            throw new FlinkRuntimeException("Container is stopped.");
+        }
+        if (metadata == null) {
+            metadata = new ClickHouseMetadata(CONTAINER, false);
+        }
+        return metadata;
+    }
+
+    @Override
+    protected DatabaseMetadata startDatabase() throws Exception {
+        CONTAINER.start();
+        return getMetadata();
+    }
+
+    @Override
+    protected void stopDatabase() throws Exception {
+        CONTAINER.stop();
+        metadata = null;
+    }
+}
