ccdeba195e10ff532a62cb65845278167911985d
David Radley <david_radley@uk.ibm.com>
2024-01-25 09:47:16 +0000

    [FLINK-33365] include filters with Lookup joins
    Co-authored-by: libenchao libenchao@gmail.com
    Co-authored-by: snuyanzin snuyanzin@gmail.com
    
    Signed-off-by: David Radley <david_radley@uk.ibm.com>

--- flink-connector-jdbc/src/test/java/org/apache/flink/connector/jdbc/table/JdbcRowDataLookupFunctionTest.java
+++ flink-connector-jdbc/src/test/java/org/apache/flink/connector/jdbc/table/JdbcRowDataLookupFunctionTest.java
@@ -28,11 +28,17 @@ import org.apache.flink.table.types.logical.LogicalType;
 import org.apache.flink.table.types.logical.RowType;
 import org.apache.flink.util.Collector;
 
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.testcontainers.shaded.com.google.common.collect.ImmutableList;
 
+import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -47,12 +53,27 @@ class JdbcRowDataLookupFunctionTest extends JdbcLookupTestBase {
             new DataType[] {
                 DataTypes.INT(), DataTypes.STRING(), DataTypes.STRING(), DataTypes.STRING()
             };
+    private static final String[] fieldNames2 =
+            new String[] {
+                "id1", "id2", "comment1", "comment2", "decimal_col", "double_col", "real_col"
+            };
+    private static final DataType[] fieldDataTypes2 =
+            new DataType[] {
+                DataTypes.INT(),
+                DataTypes.STRING(),
+                DataTypes.STRING(),
+                DataTypes.STRING(),
+                DataTypes.DECIMAL(10, 4),
+                DataTypes.DOUBLE(),
+                DataTypes.FLOAT()
+            };
 
     private static final String[] lookupKeys = new String[] {"id1", "id2"};
 
     @ParameterizedTest(name = "withFailure = {0}")
     @ValueSource(booleans = {false, true})
     void testLookup(boolean withFailure) throws Exception {
+
         JdbcRowDataLookupFunction lookupFunction = buildRowDataLookupFunction(withFailure);
 
         ListOutputCollector collector = new ListOutputCollector();
@@ -82,6 +103,130 @@ class JdbcRowDataLookupFunctionTest extends JdbcLookupTestBase {
         assertThat(result).isEqualTo(expected);
     }
 
+    @ParameterizedTest
+    @MethodSource("lookupWithPredicatesProvider")
+    void testEval(TestSpec testSpec) throws Exception {
+        JdbcRowDataLookupFunction lookupFunction =
+                buildRowDataLookupFunctionWithPredicates(
+                        testSpec.withFailure, testSpec.resolvedPredicates, testSpec.pushdownParams);
+
+        ListOutputCollector collector = new ListOutputCollector();
+        lookupFunction.setCollector(collector);
+        lookupFunction.open(null);
+        lookupFunction.eval(testSpec.keys);
+
+        if (testSpec.withFailure) {
+            // Close connection here, and this will be recovered by retry
+            if (lookupFunction.getDbConnection() != null) {
+                lookupFunction.getDbConnection().close();
+            }
+        }
+
+        List<String> result =
+                new ArrayList<>(collector.getOutputs())
+                        .stream().map(RowData::toString).sorted().collect(Collectors.toList());
+        Collections.sort(testSpec.expected);
+        assertThat(result).isEqualTo(testSpec.expected);
+    }
+
+    private static class TestSpec {
+
+        private boolean withFailure;
+        private final List<String> resolvedPredicates;
+        private final Serializable[] pushdownParams;
+        private final Object[] keys;
+        private List<String> expected;
+
+        private TestSpec(
+                boolean withFailure,
+                List<String> resolvedPredicates,
+                Serializable[] pushdownParams,
+                Object[] keys,
+                List<String> expected) {
+            this.withFailure = withFailure;
+            this.resolvedPredicates = resolvedPredicates;
+            this.pushdownParams = pushdownParams;
+            this.keys = keys;
+            this.expected = expected;
+        }
+
+        @Override
+        public String toString() {
+            return "TestSpec{"
+                    + "withFailure="
+                    + withFailure
+                    + ", resolvedPredicates="
+                    + resolvedPredicates
+                    + ", pushdownParams="
+                    + Arrays.toString(pushdownParams)
+                    + ", keys="
+                    + Arrays.toString(keys)
+                    + ", expected="
+                    + expected
+                    + '}';
+        }
+    }
+
+    static Collection<TestSpec> lookupWithPredicatesProvider() {
+        return ImmutableList.<TestSpec>builder()
+                .addAll(getTestSpecs(true))
+                .addAll(getTestSpecs(false))
+                .build();
+    }
+
+    @NotNull
+    private static ImmutableList<TestSpec> getTestSpecs(boolean withFailure) {
+        return ImmutableList.of(
+                // var char single filter
+                new TestSpec(
+                        withFailure,
+                        Collections.singletonList("(comment1 = ?)"),
+                        new Serializable[] {"11-c1-v1"},
+                        new Object[] {1, StringData.fromString("1")},
+                        Collections.singletonList("+I(1,1,11-c1-v1,11-c2-v1,100.1011,1.1,2.2)")),
+                // decimal single filter
+                new TestSpec(
+                        withFailure,
+                        Collections.singletonList("(decimal_col = ?)"),
+                        new Serializable[] {BigDecimal.valueOf(100.1011)},
+                        new Object[] {1, StringData.fromString("1")},
+                        Collections.singletonList("+I(1,1,11-c1-v1,11-c2-v1,100.1011,1.1,2.2)")),
+                // real single filter
+                new TestSpec(
+                        withFailure,
+                        Collections.singletonList("(real_col = ?)"),
+                        new Serializable[] {2.2},
+                        new Object[] {1, StringData.fromString("1")},
+                        Arrays.asList(
+                                "+I(1,1,11-c1-v1,11-c2-v1,100.1011,1.1,2.2)",
+                                "+I(1,1,11-c1-v2,11-c2-v2,100.2022,2.2,2.2)")),
+                // double single filter
+                new TestSpec(
+                        withFailure,
+                        Collections.singletonList("(double_col = ?)"),
+                        new Serializable[] {
+                            1.1,
+                        },
+                        new Object[] {1, StringData.fromString("1")},
+                        Collections.singletonList("+I(1,1,11-c1-v1,11-c2-v1,100.1011,1.1,2.2)")),
+                // and
+                new TestSpec(
+                        withFailure,
+                        Collections.singletonList("(real_col = ?) AND (double_col = ?)"),
+                        new Serializable[] {2.2, 1.1},
+                        new Object[] {1, StringData.fromString("1")},
+                        Collections.singletonList("+I(1,1,11-c1-v1,11-c2-v1,100.1011,1.1,2.2)")),
+                // or
+                new TestSpec(
+                        withFailure,
+                        Collections.singletonList("(decimal_col = ?) OR (double_col = ?)"),
+                        new Serializable[] {BigDecimal.valueOf(100.2022), 1.1},
+                        new Object[] {1, StringData.fromString("1")},
+                        Arrays.asList(
+                                "+I(1,1,11-c1-v1,11-c2-v1,100.1011,1.1,2.2)",
+                                "+I(1,1,11-c1-v2,11-c2-v2,100.2022,2.2,2.2)")));
+    }
+
     private JdbcRowDataLookupFunction buildRowDataLookupFunction(boolean withFailure) {
         InternalJdbcConnectionOptions jdbcOptions =
                 InternalJdbcConnectionOptions.builder()
@@ -103,7 +248,36 @@ class JdbcRowDataLookupFunctionTest extends JdbcLookupTestBase {
                 fieldNames,
                 fieldDataTypes,
                 lookupKeys,
-                rowType);
+                rowType,
+                Collections.emptyList(),
+                new Serializable[0]);
+    }
+
+    private JdbcRowDataLookupFunction buildRowDataLookupFunctionWithPredicates(
+            boolean withFailure, List<String> resolvedPredicates, Serializable[] pushdownParams) {
+        InternalJdbcConnectionOptions jdbcOptions =
+                InternalJdbcConnectionOptions.builder()
+                        .setDriverName(getMetadata().getDriverClass())
+                        .setDBUrl(getMetadata().getJdbcUrl())
+                        .setTableName(LOOKUP_TABLE)
+                        .build();
+
+        RowType rowType =
+                RowType.of(
+                        Arrays.stream(fieldDataTypes2)
+                                .map(DataType::getLogicalType)
+                                .toArray(LogicalType[]::new),
+                        fieldNames2);
+
+        return new JdbcRowDataLookupFunction(
+                jdbcOptions,
+                withFailure ? 1 : LookupOptions.MAX_RETRIES.defaultValue(),
+                fieldNames2,
+                fieldDataTypes2,
+                lookupKeys,
+                rowType,
+                resolvedPredicates,
+                pushdownParams);
     }
 
     private static final class ListOutputCollector implements Collector<RowData> {
--- flink-connector-jdbc/src/test/java/org/apache/flink/connector/jdbc/table/JdbcTablePlanTest.java
+++ flink-connector-jdbc/src/test/java/org/apache/flink/connector/jdbc/table/JdbcTablePlanTest.java
@@ -35,7 +35,7 @@ public class JdbcTablePlanTest extends TableTestBase {
     private TestInfo testInfo;
 
     @BeforeEach
-    public void setup(TestInfo testInfo) {
+    void setup(TestInfo testInfo) {
         this.testInfo = testInfo;
         util.tableEnv()
                 .executeSql(
@@ -52,22 +52,96 @@ public class JdbcTablePlanTest extends TableTestBase {
                                 + "  'url'='jdbc:derby:memory:test',"
                                 + "  'table-name'='test_table'"
                                 + ")");
+        util.tableEnv()
+                .executeSql(
+                        "CREATE TABLE  d ( "
+                                + "ip varchar(20), type int, age int"
+                                + ") WITH ("
+                                + "  'connector'='jdbc',"
+                                + "  'url'='jdbc:derby:memory:test1',"
+                                + "  'table-name'='d'"
+                                + ")");
+
+        util.tableEnv()
+                .executeSql(
+                        "CREATE TABLE table_with_weird_column_name ( "
+                                + "ip varchar(20), type int, ```?age:` int"
+                                + ") WITH ("
+                                + "  'connector'='jdbc',"
+                                + "  'url'='jdbc:derby:memory:test1',"
+                                + "  'table-name'='d'"
+                                + ")");
+        util.tableEnv()
+                .executeSql(
+                        "CREATE TABLE a ( "
+                                + " ip string, proctime as proctime() "
+                                + ") WITH ("
+                                + "  'connector'='jdbc',"
+                                + "  'url'='jdbc:derby:memory:test2',"
+                                + "  'table-name'='a'"
+                                + ")");
     }
 
     @Test
-    public void testProjectionPushDown() {
+    void testProjectionPushDown() {
         util.verifyExecPlan("SELECT decimal_col, timestamp9_col, id FROM jdbc");
     }
 
     @Test
-    public void testLimitPushDown() {
+    void testLimitPushDown() {
         util.verifyExecPlan("SELECT id, time_col FROM jdbc LIMIT 3");
     }
 
     @Test
-    public void testFilterPushdown() {
+    void testFilterPushdown() {
         util.verifyExecPlan(
                 "SELECT id, time_col, real_col FROM jdbc WHERE id = 900001 AND time_col <> TIME '11:11:11' OR double_col >= -1000.23");
+    }
+
+    /**
+     * Note the join condition is not present in the optimized plan, see FLINK-34170, as it is
+     * handled in the JDBC java code, where it adds the join conditions to the select statement
+     * string.
+     */
+    @Test
+    void testLookupJoin() {
+        util.verifyExecPlan(
+                "SELECT * FROM a LEFT JOIN d FOR SYSTEM_TIME AS OF a.proctime ON a.ip = d.ip");
+    }
+
+    @Test
+    void testLookupJoinWithFilter() {
+        util.verifyExecPlan(
+                "SELECT * FROM a LEFT JOIN d FOR SYSTEM_TIME AS OF a.proctime ON d.type = 0 AND a.ip = d.ip");
+    }
+
+    @Test
+    void testLookupJoinWithANDAndORFilter() {
+        util.verifyExecPlan(
+                "SELECT * FROM a LEFT JOIN d FOR SYSTEM_TIME AS OF a.proctime ON ((d.age = 50 AND d.type = 0) "
+                        + "OR (d.type = 1 AND d.age = 40)) AND a.ip = d.ip");
+    }
+
+    @Test
+    void testLookupJoinWith2ANDsAndORFilter() {
+        util.verifyExecPlan(
+                "SELECT * FROM a JOIN d FOR SYSTEM_TIME AS OF a.proctime "
+                        + "ON ((50 > d.age AND d.type = 1 AND d.age > 0 ) "
+                        + "OR (70 > d.age AND d.type = 6 AND d.age > 10)) AND a.ip = d.ip");
+    }
+
+    @Test
+    void testLookupJoinWithORFilter() {
+        util.verifyExecPlan(
+                "SELECT * FROM a LEFT JOIN d FOR SYSTEM_TIME AS OF a.proctime ON (d.age = 50 OR d.type = 1) AND a.ip = d.ip");
+    }
+
+    @Test
+    void testLookupJoinWithWeirdColumnNames() {
+        util.verifyExecPlan(
+                "SELECT * FROM a LEFT JOIN table_with_weird_column_name FOR SYSTEM_TIME AS OF a.proctime "
+                        + "ON (table_with_weird_column_name.```?age:` = 50 OR table_with_weird_column_name.type = 1) "
+                        + "AND a.ip = table_with_weird_column_name.ip");
     }
 
     /**
