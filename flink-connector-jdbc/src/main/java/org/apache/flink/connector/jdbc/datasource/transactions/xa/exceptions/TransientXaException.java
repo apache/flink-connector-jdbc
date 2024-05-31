b955e8b5479c65512f94ee7270eb2868a4a14ebe
Joao Boto <boto@boto.pro>
2024-05-31 10:58:34 +0200

    [FLINK-35496] Fix annotations on new JdbcSink and JdbcSource

--- flink-connector-jdbc/src/main/java/org/apache/flink/connector/jdbc/datasource/transactions/xa/exceptions/TransientXaException.java
+++ flink-connector-jdbc/src/main/java/org/apache/flink/connector/jdbc/datasource/transactions/xa/exceptions/TransientXaException.java
@@ -1,6 +1,6 @@
 package org.apache.flink.connector.jdbc.datasource.transactions.xa.exceptions;
 
-import org.apache.flink.annotation.PublicEvolving;
+import org.apache.flink.annotation.Experimental;
 import org.apache.flink.util.FlinkRuntimeException;
 
 import javax.transaction.xa.XAException;
@@ -9,7 +9,7 @@ import javax.transaction.xa.XAException;
  * Indicates a transient or unknown failure from the resource manager (see {@link
  * XAException#XA_RBTRANSIENT XA_RBTRANSIENT}, {@link XAException#XAER_RMFAIL XAER_RMFAIL}).
  */
-@PublicEvolving
+@Experimental
 public class TransientXaException extends FlinkRuntimeException {
     public TransientXaException(XAException cause) {
         super(cause);
