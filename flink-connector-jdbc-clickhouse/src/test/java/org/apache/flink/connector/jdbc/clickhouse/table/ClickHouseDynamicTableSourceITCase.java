d8c5b36656cc0ef1017394d46d937188fe6a6d90
Ares Gal <arsenag6@gmail.com>
2025-05-28 07:03:03 +0300

    fixed checkstyle

--- flink-connector-jdbc-clickhouse/src/test/java/org/apache/flink/connector/jdbc/clickhouse/table/ClickHouseDynamicTableSourceITCase.java
+++ flink-connector-jdbc-clickhouse/src/test/java/org/apache/flink/connector/jdbc/clickhouse/table/ClickHouseDynamicTableSourceITCase.java
@@ -25,6 +25,7 @@ import org.apache.flink.connector.jdbc.testutils.tables.TableBuilder;
 import org.apache.flink.connector.jdbc.testutils.tables.TableRow;
 import org.apache.flink.table.api.DataTypes;
 import org.apache.flink.types.Row;
+
 import org.junit.jupiter.api.Disabled;
 
 import java.math.BigDecimal;
