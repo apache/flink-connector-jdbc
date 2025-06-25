20e16af796617aea67ee8dcf881408e0c31cd9fc
zm999999 <15247297639@163.com>
2025-06-25 17:35:18 +0800

    [hotfix][improvement][connector/jdbc] Merge pull request #155 from apache/main Optimize the Postgresql upsert query to avoid unnecessary execution effort

--- flink-connector-jdbc/src/test/java/org/apache/flink/connector/jdbc/databases/postgres/dialect/PostgresDialectTypeTest.java
+++ flink-connector-jdbc/src/test/java/org/apache/flink/connector/jdbc/databases/postgres/dialect/PostgresDialectTypeTest.java
@@ -20,8 +20,12 @@ package org.apache.flink.connector.jdbc.databases.postgres.dialect;
 
 import org.apache.flink.connector.jdbc.dialect.JdbcDialectTypeTest;
 
+import org.junit.jupiter.api.Test;
+
 import java.util.Arrays;
 import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /** The PostgresSql params for {@link JdbcDialectTypeTest}. */
 public class PostgresDialectTypeTest extends JdbcDialectTypeTest {
@@ -61,5 +65,25 @@ public class PostgresDialectTypeTest extends JdbcDialectTypeTest {
                         "TIMESTAMP(9) WITHOUT TIME ZONE",
                         "The precision of field 'f0' is out of the TIMESTAMP precision range [1, 6] supported by PostgreSQL dialect."),
                 createTestItem("TIMESTAMP_LTZ(3)", "Unsupported type:TIMESTAMP_LTZ(3)"));
+    }
+
+    @Test
+    void testUpsertStatement() {
+        PostgresDialect dialect = new PostgresDialect();
+        final String tableName = "tbl";
+        final String[] fieldNames = {
+            "id", "name", "email", "ts", "field1", "field_2", "__field_3__"
+        };
+        final String[] doUpdatekeyFields = {"id", "__field_3__"};
+        final String[] doNothingkeyFields = {
+            "id", "name", "email", "ts", "field1", "field_2", "__field_3__"
+        };
+
+        assertThat(dialect.getUpsertStatement(tableName, fieldNames, doUpdatekeyFields).get())
+                .isEqualTo(
+                        "INSERT INTO tbl(id, name, email, ts, field1, field_2, __field_3__) VALUES (:id, :name, :email, :ts, :field1, :field_2, :__field_3__) ON CONFLICT (id, __field_3__) DO UPDATE SET name=EXCLUDED.name, email=EXCLUDED.email, ts=EXCLUDED.ts, field1=EXCLUDED.field1, field_2=EXCLUDED.field_2");
+        assertThat(dialect.getUpsertStatement(tableName, fieldNames, doNothingkeyFields).get())
+                .isEqualTo(
+                        "INSERT INTO tbl(id, name, email, ts, field1, field_2, __field_3__) VALUES (:id, :name, :email, :ts, :field1, :field_2, :__field_3__) ON CONFLICT (id, name, email, ts, field1, field_2, __field_3__) DO NOTHING");
     }
 }
