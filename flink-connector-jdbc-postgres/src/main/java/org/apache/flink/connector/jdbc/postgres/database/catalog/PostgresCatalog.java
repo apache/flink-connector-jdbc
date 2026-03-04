fa1a332c5dee389d77d07c756ca697ba298c4487
Chan hae OH <45858414+och5351@users.noreply.github.com>
2026-03-04 15:04:30 +0900

    [FLINK-38851] Support passing arbitrary database options to JDBC Catalog (#183)
    [FLINK-38851] Support passing arbitrary database options to JDBC Catalog
    
    Co-authored-by: och5351 <shining@kia.com>
    (cherry picked from commit 414d7c018a8646eeaf7a47295e0e4067c032a9a6)

--- flink-connector-jdbc-postgres/src/main/java/org/apache/flink/connector/jdbc/postgres/database/catalog/PostgresCatalog.java
+++ flink-connector-jdbc-postgres/src/main/java/org/apache/flink/connector/jdbc/postgres/database/catalog/PostgresCatalog.java
@@ -187,7 +187,7 @@ public class PostgresCatalog extends AbstractJdbcCatalog {
             throw new DatabaseNotExistException(getName(), databaseName);
         }
 
-        final String url = getDatabaseUrl(databaseName);
+        final String url = urlFunction.apply(databaseName);
         try (Connection conn = DriverManager.getConnection(url, connectionProperties)) {
             // get all schemas
             List<String> schemas;
