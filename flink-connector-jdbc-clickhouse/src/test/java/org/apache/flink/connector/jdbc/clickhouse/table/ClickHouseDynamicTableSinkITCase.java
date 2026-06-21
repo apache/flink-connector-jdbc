0a1ea6d2be45ab66341ce7a5ef51a9ced6b49373
arseneag <arsenag6@gmail.com>
2026-06-21 03:42:01 +0300

    fixing test 9

--- flink-connector-jdbc-clickhouse/src/test/java/org/apache/flink/connector/jdbc/clickhouse/table/ClickHouseDynamicTableSinkITCase.java
+++ flink-connector-jdbc-clickhouse/src/test/java/org/apache/flink/connector/jdbc/clickhouse/table/ClickHouseDynamicTableSinkITCase.java
@@ -23,24 +23,11 @@ import org.apache.flink.connector.jdbc.clickhouse.database.dialect.ClickHouseDia
 import org.apache.flink.connector.jdbc.core.table.sink.JdbcDynamicTableSinkITCase;
 import org.apache.flink.connector.jdbc.testutils.tables.TableRow;
 import org.apache.flink.table.api.DataTypes;
-import org.apache.flink.table.api.EnvironmentSettings;
-import org.apache.flink.table.api.TableEnvironment;
-import org.apache.flink.table.planner.factories.TestValuesTableFactory;
-import org.apache.flink.types.Row;
-import org.apache.flink.types.RowKind;
-
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
 
 import static org.apache.flink.connector.jdbc.clickhouse.ClickHouseTestBase.tableRow;
 import static org.apache.flink.connector.jdbc.testutils.tables.TableBuilder.dbType;
 import static org.apache.flink.connector.jdbc.testutils.tables.TableBuilder.field;
 import static org.apache.flink.connector.jdbc.testutils.tables.TableBuilder.pkField;
-import static org.assertj.core.api.Assertions.assertThat;
 
 /** The Table Sink ITCase for {@link ClickHouseDialect}. */
 class ClickHouseDynamicTableSinkITCase extends JdbcDynamicTableSinkITCase
@@ -79,12 +66,6 @@ class ClickHouseDynamicTableSinkITCase extends JdbcDynamicTableSinkITCase
     }
 
     @Override
-    protected TableRow createCheckpointOutputTable() {
-        return tableRow(
-                "checkpointTable", field("id", dbType("Int64"), DataTypes.BIGINT().notNull()));
-    }
-
-    @Override
     protected TableRow createUserOutputTable() {
         return tableRow(
                 "USER_TABLE",
@@ -92,164 +73,11 @@ class ClickHouseDynamicTableSinkITCase extends JdbcDynamicTableSinkITCase
                 pkField("user_name", dbType("String"), DataTypes.VARCHAR(20).notNull()),
                 field("email", dbType("String"), DataTypes.VARCHAR(255)),
                 field("balance", dbType("Decimal(18, 2)"), DataTypes.DECIMAL(18, 2)),
-                field("entry_dttm", dbType("DateTime64(3)"), DataTypes.TIMESTAMP()),
-                field(
-                        "other_emails_array",
-                        dbType("Array(String)"),
-                        DataTypes.ARRAY(DataTypes.STRING())),
-                field("other_ids_array", dbType("Array(Int32)"), DataTypes.ARRAY(DataTypes.INT())),
-                field(
-                        "add_info_map",
-                        dbType("Map(Int32, String)"),
-                        DataTypes.MAP(DataTypes.INT(), DataTypes.STRING())),
-                field("last_log_date", dbType("Date"), DataTypes.DATE()),
                 field("balance2", dbType("Decimal(18, 2)"), DataTypes.DECIMAL(18, 2)));
     }
 
     @Override
-    protected List<Row> testUserData() {
-        return Arrays.asList(
-                Row.of(
-                        "user1",
-                        "Tom",
-                        "tom123@gmail.com",
-                        new BigDecimal("8.10"),
-                        LocalDateTime.parse("1999-06-08T10:12:11.301"),
-                        new String[] {"tom3@gmail.com", "tom5@gmail.com"},
-                        new Integer[] {81723, 12315},
-                        new HashMap<Integer, String>() {
-                            {
-                                put(333, "A");
-                                put(444, "B");
-                            }
-                        },
-                        LocalDate.parse("2026-05-05"),
-                        new BigDecimal("16.20")),
-                Row.of(
-                        "user3",
-                        "Bailey",
-                        "bailey@qq.com",
-                        new BigDecimal("9.99"),
-                        LocalDateTime.parse("1999-12-11T20:22:11.301"),
-                        new String[] {"bll3@gmail.com", "bll5@gmail.com"},
-                        new Integer[] {81623, 22371},
-                        new HashMap<Integer, String>() {
-                            {
-                                put(111, "C");
-                                put(666, "D");
-                            }
-                        },
-                        LocalDate.parse("2026-05-10"),
-                        new BigDecimal("19.98")),
-                Row.of(
-                        "user4",
-                        "Tina",
-                        "tina@gmail.com",
-                        new BigDecimal("11.30"),
-                        LocalDateTime.parse("2001-01-01T00:11:44.124"),
-                        new String[] {"tina1@gmail.com", "tina6@gmail.com"},
-                        new Integer[] {12415, 66423},
-                        new HashMap<Integer, String>() {
-                            {
-                                put(999, "X");
-                                put(222, "N");
-                            }
-                        },
-                        LocalDate.parse("2026-01-05"),
-                        new BigDecimal("22.60")));
-    }
-
-    @Override
-    protected void testReadingFromChangelogSource() throws Exception {
-        TableEnvironment tEnv = TableEnvironment.create(EnvironmentSettings.newInstance().build());
-        String dataId =
-                TestValuesTableFactory.registerData(
-                        Arrays.asList(
-                                Row.ofKind(
-                                        RowKind.INSERT,
-                                        "user1",
-                                        "Tom",
-                                        "tom123@gmail.com",
-                                        new BigDecimal("8.10"),
-                                        LocalDateTime.parse("1999-06-08T10:12:11.301"),
-                                        new String[] {"tom3@gmail.com", "tom5@gmail.com"},
-                                        new Integer[] {81723, 12315},
-                                        new HashMap<Integer, String>() {
-                                            {
-                                                put(333, "A");
-                                                put(444, "B");
-                                            }
-                                        },
-                                        LocalDate.parse("2026-05-05")),
-                                Row.ofKind(
-                                        RowKind.INSERT,
-                                        "user3",
-                                        "Bailey",
-                                        "bailey@qq.com",
-                                        new BigDecimal("9.99"),
-                                        LocalDateTime.parse("1999-12-11T20:22:11.301"),
-                                        new String[] {"bll3@gmail.com", "bll5@gmail.com"},
-                                        new Integer[] {81623, 22371},
-                                        new HashMap<Integer, String>() {
-                                            {
-                                                put(111, "C");
-                                                put(666, "D");
-                                            }
-                                        },
-                                        LocalDate.parse("2026-05-10")),
-                                Row.ofKind(
-                                        RowKind.INSERT,
-                                        "user4",
-                                        "Tina",
-                                        "tina@gmail.com",
-                                        new BigDecimal("11.30"),
-                                        LocalDateTime.parse("2001-01-01T00:11:44.124"),
-                                        new String[] {"tina1@gmail.com", "tina6@gmail.com"},
-                                        new Integer[] {12415, 66423},
-                                        new HashMap<Integer, String>() {
-                                            {
-                                                put(999, "X");
-                                                put(222, "N");
-                                            }
-                                        },
-                                        LocalDate.parse("2026-01-05"))));
-
-        String userTableLogs = "user_logs";
-        tEnv.executeSql(
-                String.format(
-                        "CREATE TABLE %s ( "
-                                + "  user_id STRING, "
-                                + "  user_name STRING, "
-                                + "  email STRING, "
-                                + "  balance DECIMAL(18,2), "
-                                + "  entry_dttm TIMESTAMP(3), "
-                                + "  other_emails_array ARRAY<STRING>, "
-                                + "  other_ids_array ARRAY<INT>, "
-                                + "  add_info_map MAP<INT, STRING>, "
-                                + "  last_log_date DATE, "
-                                + "  balance2 AS balance * 2 "
-                                + ") WITH ( "
-                                + " 'connector' = 'values', "
-                                + " 'data-id' = '%s', "
-                                + " 'changelog-mode' = 'I,UA,UB,D' "
-                                + ")",
-                        userTableLogs, dataId));
-
-        String userTableSink = "user_sink";
-        tEnv.executeSql(
-                userOutputTable.getCreateQueryForFlink(
-                        getMetadata(),
-                        userTableSink,
-                        Arrays.asList(
-                                "'sink.buffer-flush.max-rows' = '2'",
-                                "'sink.buffer-flush.interval' = '0'")));
-
-        tEnv.executeSql(
-                        String.format(
-                                "INSERT INTO %s SELECT * FROM %s", userTableSink, userTableLogs))
-                .await();
-
-        assertThat(userOutputTable.selectAllTable(getMetadata()))
-                .containsExactlyInAnyOrderElementsOf(testUserData());
+    protected TableRow createCheckpointOutputTable() {
+        return tableRow("checkpointTable", field("id", dbType("Int64"), DataTypes.BIGINT().notNull()));
     }
 }
